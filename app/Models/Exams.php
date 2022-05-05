9c7720b625f9e4655fc039d625a623c9ceeee1a1
Sergio Puchades Martinez <spuchadesm@uoc.edu>
2022-05-05 17:28:14 +0200

    timestamp

--- app/Models/Exams.php
+++ app/Models/Exams.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Exams extends Model
 {
     use HasFactory;
+    public $timestamps = false;
     protected $table = 'exams';
     protected $primaryKey = 'id_exams';
 
