766de6136386c8b67bc73145301c7772bd3ef9dc
Manuel Yerbes <mayerga4@gmail.com>
2022-05-05 22:42:31 +0200

    EXAMS -> timestamps = false

--- app/Models/Exams.php
+++ app/Models/Exams.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Exams extends Model
 {
     use HasFactory;
-    // public $timestamps = false;
+    public $timestamps = false;
     protected $table = 'exams';
     protected $primaryKey = 'id_exam';
 
