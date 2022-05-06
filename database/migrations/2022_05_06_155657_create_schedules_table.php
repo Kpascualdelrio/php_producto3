47f868b9fddac6d8b3726b1e6198ce5f7c9f3595
Sergio Puchades Martinez <spuchadesm@uoc.edu>
2022-05-06 18:16:42 +0200

    migrate

---
+++ database/migrations/2022_05_06_155657_create_schedules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_class')->references('id')->on('asignaturas');
+            $table->dateTime('time_start');
+            $table->dateTime('time_end');
+            $table->date('day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
